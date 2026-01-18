14055713e13df16369f301d6eaf6c5325471af9f
SomeshwarJoshi <24071a0532@vnrvjiet.in>
2026-01-18 18:33:09 +0530

    ..

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
